17222cfc08392d7e2b4cbea15c67f228eac3dabf
Chuniversity <contact@chunis.org>
2021-10-07 23:10:24 -0400

    remove console.logs

--- client/src/components/Result.js
+++ client/src/components/Result.js
@@ -7,7 +7,6 @@ const Result = (props) => {
 	if (props.permit === 'all') return true;
 		else return cleaned === props.permit;
 	});
-	console.log('filtered results', filteredResults)
 	return (
 		<>
 			<div className="results-filters">
@@ -50,7 +49,6 @@ const Result = (props) => {
 						<Link key={site.attributes.OBJECTID} to={`/places/${props.placeId}/sites/${site.attributes.OBJECTID}`}>
 							<div className="inner-wrapper" key={index}>
 								<div className="image-wrapper" key={index}>
-									{/* {console.log(`https://maps.googleapis.com/maps/api/streetview?size=400x400&location=${site.attributes.HNUM}+${site.attributes.ST_NAME}+${site.attributes.PRE_DIR}+${site.attributes.ST_TYPE}+MIAMI+FL&heading=271&pitch=-0.76&key=${process.env.REACT_APP_GOOGLE_API_KEY}`)} */}
 									<img className="google-image" src={`https://maps.googleapis.com/maps/api/streetview?size=400x400&location=${site.attributes.HNUM}+${site.attributes.ST_NAME}+${site.attributes.PRE_DIR}+${site.attributes.ST_TYPE}+MIAMI+FL&heading=271&pitch=-0.76&key=${process.env.REACT_APP_GOOGLE_API_KEY}`} alt="contaminated site" />
 								</div>
 								<div className="text-wrapper">
